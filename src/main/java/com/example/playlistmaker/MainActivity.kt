390c027b407e401d46e68d0201c995f95aa74b29
nvkornyakov <sj.nike@bk.ru>
2023-04-24 16:14:20 +0300

    Revert "Задачи спринта №8"
    This reverts commit 42adb5cfcb068bd112b6ab110650a5385a4161f1.

--- src/main/java/com/example/playlistmaker/MainActivity.kt
+++ src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,38 +1,11 @@
 package com.example.playlistmaker
 
-import android.annotation.SuppressLint
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId", "SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val button1: View = findViewById(R.id.search_button)
-        button1.setOnClickListener {
-            val intent = Intent(this@MainActivity, SearchActivity::class.java)
-            startActivity(intent)
-        }
-
-        val button2: View = findViewById(R.id.media_button)
-        button2.setOnClickListener {
-            val intent = Intent(this@MainActivity, MediaActivity::class.java)
-            startActivity(intent)
-        }
-
-        val button3: View = findViewById(R.id.settings_button_container)
-        button3.setOnClickListener {
-            val intent = Intent(this@MainActivity, SettingsActivity::class.java)
-            startActivity(intent)
-        }
-
     }
 }
-
-
-
-
