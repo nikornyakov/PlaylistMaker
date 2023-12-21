2808c7adb36f436e04b6bbb01b4181f71eba2b9e
nvkornyakov <sj.nike@bk.ru>
2023-12-21 19:18:24 +0300

    Sprint 11

--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -1,8 +1,20 @@
 package com.example.playlistmaker
 
+import java.text.SimpleDateFormat
+import java.util.Locale
+
 data class Track(
     val trackName: String, // Название композиции
     val artistName: String, // Имя исполнителя
-    val trackTime: String, // Продолжительность трека
+    val trackTimeMillis: Long, // Продолжительность трека
     val artworkUrl100: String // Ссылка на изображение обложки
+){
+    val trackTime: String
+        get(){return SimpleDateFormat("mm:ss", Locale.getDefault()).format(this.trackTimeMillis)}
+}
+
+class TrackResponse(
+    val results: ArrayList<Track>,
+    val resultCount: Int
 )
+val songs = ArrayList<Track>()
